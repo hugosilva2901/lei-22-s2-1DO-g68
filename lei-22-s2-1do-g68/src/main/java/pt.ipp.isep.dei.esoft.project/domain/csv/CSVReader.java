4bab6b86ecedd59c45b68a9da1a55b83e88a7eb4
TiagoSilva_ <1221698@isep.ipp.pt>
2023-06-04 15:36:04 +0100

    CSV reader a ler todas as variaveis e com um caminho relativo

--- lei-22-s2-1do-g68/src/main/java/pt.ipp.isep.dei.esoft.project/domain/csv/CSVReader.java
+++ lei-22-s2-1do-g68/src/main/java/pt.ipp.isep.dei.esoft.project/domain/csv/CSVReader.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 
 public class CSVReader {
     public static void main(String[] args) {
-        String csvFile = "C:\\Users\\tiago\\OneDrive\\Ambiente de Trabalho\\esoft\\Projeto Integrador\\lei-22-s2-1do-g68\\src\\main\\java\\pt.ipp.isep.dei.esoft.project\\domain\\csv\\legacyRealStateUSAMoodle_MATCP_MDISC.csv";
+        String csvFile = "lei-22-s2-1do-g68\\src\\main\\java\\pt.ipp.isep.dei.esoft.project\\domain\\csv\\legacyRealStateUSAMoodle_MATCP_MDISC.csv";
         String line = "";
         String cvsSplitBy = ";";
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
@@ -15,10 +15,37 @@ public class CSVReader {
             while ((line = br.readLine()) != null) {
                 String[] data = line.split(cvsSplitBy);
                 if(i>0) {
-                    String type = data[6];
-                    String area = data[7];
-                    String distance = data[9];
-                    // use data here
+                    String id = data[0];
+                    String owner_name = data [1];
+                    String owner_passportNum = data [2];
+                    String owner_TIN = data [3];
+                    String owner_email = data [4];
+                    String owner_phone = data [5];
+                    String property_type = data [6];
+                    String property_area= data [7];
+                    String property_location = data [8];
+                    String property_distanceFromCenter = data [9];
+                    String property_numberBedrooms = data [10];
+                    String property_numberBathrooms = data [11];
+                    String property_pnumParking = data [12];
+                    String property_centralHeating = data [13];
+                    String property_airconditioned = data [14];
+                    String property_basement = data [15];
+                    String property_loft = data [16];
+                    String property_sunExposure = data [17];
+                    String property_requested_sale_rent_price = data [18];
+                    String property_sale_rent_price = data [19];
+                    String commission = data [20];
+                    String contract_duration = data [21];
+                    String property_dateAnnounceRequest = data [22];
+                    String property_dateofSale = data [23];
+                    String type_business = data [24];
+                    String store_ID = data [25];
+                    String store_name = data [26];
+                    String store_location = data [27];
+                    String store_phonenumber = data [28];
+                    String store_emailAddress = data [29];
+
                 }
                 i++;
             }
