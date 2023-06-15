6270ce38c04990715d48653b4f2b91614026288d
Miguel_89 <1221678@isep.ipp.pt>
2023-06-15 18:14:13 +0100

    IMPLEMENTS US020 ui and controller

--- lei-22-s2-1do-g68/src/main/java/pt.ipp.isep.dei.esoft.project/ui/console/MessageVisitResponseUI.java
+++ lei-22-s2-1do-g68/src/main/java/pt.ipp.isep.dei.esoft.project/ui/console/MessageVisitResponseUI.java
@@ -1,5 +1,113 @@
 package pt.ipp.isep.dei.esoft.project.ui.console;
 
+import pt.ipp.isep.dei.esoft.project.application.controller.MessageVisitResponseController;
+import pt.ipp.isep.dei.esoft.project.domain.MessageVisit;
+
+import java.util.List;
+import java.util.Scanner;
+
 public class MessageVisitResponseUI {
+
+    MessageVisitResponseController controller = new MessageVisitResponseController();
+    private MessageVisit messageVisit;
+
+    @Override
+    public void run() {
+        System.out.println("Do you want to accept or reject a Visit Request?");
+        System.out.println("1 - Accept");
+        System.out.println("2 - Reject");
+        System.out.println("0 - Exit");
+        int answer = -1;
+
+        Scanner input = new Scanner(System.in);
+
+        while (answer < 0 || answer > 2) {
+            System.out.println("Select an option:");
+            answer = input.nextInt();
+        }
+
+        switch (answer) {
+            case 1:
+                runAccept();
+                break;
+            case 2:
+                runReject();
+                break;
+            case 0:
+                break;
+        }
+    }
+
+    private void runAccept() {
+        messageVisit= displayAndSelectMessageVisit();
+        if(messageVisit==null){
+            System.out.println("There are no Visit messages to show.\n");
+            return;
+        }
+
+        String id = controller.getVisitRequest();
+        String message = RequestmessageConfimation();
+        String messagefinal= controller.acceptorRejectVisitRequest(id, message, messageVisit);
+        System.out.println(messagefinal);
+    }
+
+    private void runReject() {
+        messageVisit= displayAndSelectMessageVisit();
+        if(messageVisit==null){
+            System.out.println("There are no Visit messages to show.\n");
+            return;
+        }
+
+        String id = controller.getVisitRequest();
+        String message = RequestmessageConfimation();
+        String messagefinal= controller.acceptorRejectVisitRequest(id, message, messageVisit);
+        System.out.println(messagefinal);
+    }
+
+
+
+    private MessageVisit displayAndSelectMessageVisit() {
+
+        List<MessageVisit> messageVisit = controller.getVisitRequest();
+
+        int listSize = messageVisit.size();
+        if (listSize == 0) {
+            System.out.println("There are no Visit messages to show.\n");
+            return null;
+        }
+        int answer = -1;
+
+        Scanner input = new Scanner(System.in);
+
+        while (answer < 1 || answer > listSize) {
+            displayMessageVisit(messageVisit);
+            System.out.println("Select a store:");
+            answer = input.nextInt();
+        }
+
+        MessageVisit description = messageVisit.get(answer - 1);
+        return description;
+
+    }
+
+    private void displayMessageVisit(List<MessageVisit> messageVisit){
+        int i=1;
+        for (MessageVisit messageVisit1 : messageVisit) {
+            System.out.println(""+i+" "+messageVisit1.toString());
+            i++;
+        }
+
+    }
+
+    private String RequestmessageConfimation(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Write the message confirmation:");
+        String message = input.nextLine();
+        return message;
+    }
+
+
+
+
 
 }
